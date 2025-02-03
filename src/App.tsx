433f988d053b522db2155b1a9369bb7ba3b9615d
Sajad <sajadsoorni@gmail.com>
2025-02-03 19:56:57 +0000

    feat: update drink price using state and spread operator
    - Implemented useState to manage drink object
    - Used the spread operator to update price while preserving other properties
    - Added a button to trigger the price update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import Message from './components/Message';
-const App = () => {
+import { useState } from 'react';
+
+function App() {
+  const [drink, setDrink] = useState({
+    title: 'Americano',
+    price: 5,
+  });
+
+  const handleClick = () => {
+    const newDrink = {
+      // title: drink.title,
+      ...drink,
+      price: 6,
+    };
+
+    setDrink(newDrink);
+  };
+
   return (
     <div>
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
-};
+}
 
 export default App;
