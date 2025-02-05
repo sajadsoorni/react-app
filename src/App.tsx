53e6f5b321b72f6793f983195651daf35073ad73
Sajad <sajadsoorni@gmail.com>
2025-02-05 20:51:21 +0000

    feat: add ExpenseFilter component for filtering expenses by category
    This commit introduces the ExpenseFilter component, allowing users to filter expenses based on category selection. It includes:
    
    - A dropdown menu with category options.
    - A callback function to update the selected category.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
 import Form from './components/Form';
 import ExpenseList from './expense-tracker/components/ExpenseList';
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
     { id: 2, description: 'bbb', amount: 20, category: 'Groceries' },
@@ -9,10 +11,13 @@ function App() {
     { id: 4, description: 'ddd', amount: 40, category: 'Transport' },
   ]);
 
+  const visibleExpenses = selectedCategory ? expenses.filter((e) => e.category === selectedCategory) : expenses;
+
   return (
     <div>
       <Form />
-      <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))} />
+      <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
+      <ExpenseList expenses={visibleExpenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))} />
     </div>
   );
 }
