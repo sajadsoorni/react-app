64ea7ee2ccb109d286ea5ec00cdc7e65250c64d4
Sajad <sajadsoorni@gmail.com>
2025-02-06 13:35:48 +0000

    Refactor App component to include ExpenseForm and centralize categories.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
-import Form from './components/Form';
 import ExpenseList from './expense-tracker/components/ExpenseList';
+import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
+import ExpenseForm from './expense-tracker/components/ExpenseForm';
+
+export const categories = ['Groceries', 'Utilities', 'Entertainment'];
+
 function App() {
   const [selectedCategory, setSelectedCategory] = useState('');
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
     { id: 2, description: 'bbb', amount: 20, category: 'Groceries' },
@@ -15,7 +19,9 @@ function App() {
 
   return (
     <div>
-      <Form />
+      <div className='mb-3'>
+        <ExpenseForm />
+      </div>
       <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
       <ExpenseList expenses={visibleExpenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))} />
     </div>
