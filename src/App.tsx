0308bf3f2294c310987e10b5c9669b301b837fa5
Sajad <sajadsoorni@gmail.com>
2025-02-14 23:17:34 +0000

    feat: add update user functionality with optimistic update

--- src/App.tsx
+++ src/App.tsx
@@ -47,12 +47,22 @@ function App() {
     setUsers([newUser, ...users]);
 
     axios
-      .post('https://jsonplaceholder.typicode.com/users', newUser)
+      .post('https://jsonplaceholder.typicode.com/users/', newUser)
       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch((err) => {
         setError(err.message);
         setUsers(originalUsers);
       });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + '!' };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
   };
 
   return (
@@ -65,10 +75,15 @@ function App() {
       <ul className='list-group'>
         {users.map((user) => (
           <li key={user.id} className='list-group-item d-flex justify-content-between align-items-center '>
-            {user.name}{' '}
-            <button className='btn btn-outline-danger' onClick={() => deleteUser(user)}>
-              Delete
-            </button>
+            {user.name}
+            <div>
+              <button className='btn btn-outline-secondary mx-2' onClick={() => updateUser(user)}>
+                Update
+              </button>
+              <button className='btn btn-outline-danger' onClick={() => deleteUser(user)}>
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
